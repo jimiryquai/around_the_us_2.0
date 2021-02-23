c9481faf1ef99c87317d21ef4bc4d528d845be3b
James Ryan <james.ryan@pathwayforcare.com>
2021-02-23 19:34:24 +0000

    Added six initial cards

--- js/index.js
+++ js/index.js
@@ -8,8 +8,54 @@ const profileJob = document.querySelector('.profile__job');
 const formInputName = document.querySelector('.form__input_name');
 const formInputJob = document.querySelector('.form__input_job');
 const formEdit = document.querySelector('.form_type_edit');
+const cardsContainer = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.card-template').content;
+
+// Cards array
+const initialCards = [
+  {
+    name: 'Yosemite Valley',
+    link: 'https://code.s3.yandex.net/web-code/yosemite.jpg',
+  },
+  {
+    name: 'Lake Louise',
+    link: 'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+  },
+  {
+    name: 'Bald Mountains',
+    link: 'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+  },
+  {
+    name: 'Latemar',
+    link: 'https://code.s3.yandex.net/web-code/latemar.jpg',
+  },
+  {
+    name: 'Vanoise National Park',
+    link: 'https://code.s3.yandex.net/web-code/vanoise.jpg',
+  },
+  {
+    name: 'Lago di Braies',
+    link: 'https://code.s3.yandex.net/web-code/lago.jpg',
+  },
+];
 
 // Reusable functions
+const createCard = card => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+  return cardElement;
+};
+
+const renderCard = card => {
+  cardsContainer.prepend(createCard(card));
+};
+
+initialCards.forEach(card => renderCard(card));
+
 const popupToggle = () => {
   popupOverlay.classList.toggle('popup_opened');
 };
