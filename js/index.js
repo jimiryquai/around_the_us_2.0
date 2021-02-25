b2f8bcb5bbec3b833518144d0798cbd5b197a9d4
James Ryan <james.ryan@pathwayforcare.com>
2021-02-25 20:24:20 +0000

    Added image popup

--- js/index.js
+++ js/index.js
@@ -4,7 +4,12 @@ const buttonEdit = document.querySelector('.button_edit');
 const buttonAdd = document.querySelector('.button_add');
 const popupEdit = document.querySelector('.popup_type_edit');
 const popupAdd = document.querySelector('.popup_type_add');
-const popupsClose = document.querySelectorAll('.popup__close');
+const popupImg = document.querySelector('.popup_type_image');
+const popupEditClose = document.querySelector('.popup__close_type_edit');
+const popupAddClose = document.querySelector('.popup__close_type_add');
+const popupImgClose = document.querySelector('.popup__close_type_image');
+const popupFigImg = popupImg.querySelector('.popup__image');
+const popupFigCaption = popupImg.querySelector('.popup__caption');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const formInputName = document.querySelector('.form__input_name');
@@ -49,14 +54,23 @@ const createCard = card => {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
-  //const buttonTrash = cardElement.querySelector('.button_trash');
+  const buttonTrash = cardElement.querySelector('.button_trash');
   const buttonHeart = cardElement.querySelector('.button_heart');
+
   cardImage.src = card.link;
   cardImage.alt = card.name;
   cardTitle.textContent = card.name;
 
+  cardImage.addEventListener('click', e => {
+    popupImg.classList.toggle('popup_opened');
+    renderPopupImg(e);
+  });
+
+  buttonTrash.addEventListener('click', e => {
+    e.target.parentElement.remove();
+  });
+
   buttonHeart.addEventListener('click', e => {
-    console.log(e.target);
     e.target.classList.toggle('button_heart_liked');
   });
 
@@ -68,6 +82,12 @@ const renderCard = card => cardsContainer.prepend(createCard(card));
 initialCards.forEach(card => renderCard(card));
 
 const popupToggle = popup => popup.classList.toggle('popup_opened');
+
+const renderPopupImg = e => {
+  popupFigImg.src = e.target.src;
+  popupFigImg.alt = e.target.alt;
+  popupFigCaption.textContent = e.target.alt;
+};
 
 const editFormLoadHandler = () => {
   formInputName.value = profileName.textContent;
@@ -106,13 +126,16 @@ formAdd.addEventListener('submit', () => {
   popupToggle(popupAdd);
 });
 
-popupsClose.forEach(popup => {
-  popup.addEventListener('click', e => {
-    e.target.classList.contains('popup__close_type_edit') &&
-    !e.target.classList.contains('popup__close_type_image')
-      ? popupToggle(popupEdit)
-      : popupToggle(popupAdd);
-  });
+popupEditClose.addEventListener('click', () => {
+  popupToggle(popupEdit);
+});
+
+popupAddClose.addEventListener('click', () => {
+  popupToggle(popupAdd);
+});
+
+popupImgClose.addEventListener('click', () => {
+  popupToggle(popupImg);
 });
 
 // Form handling
