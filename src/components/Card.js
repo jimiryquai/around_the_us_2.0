085f6ecee0f5d4307a6dd3bfbe6d9fe3d6681817
James Ryan <james.ryan@pathwayforcare.com>
2021-03-11 06:13:32 +0000

    created components folder

---
+++ src/components/Card.js
@@ -0,0 +1,82 @@
+import { popupConfig, cardConfig } from '../index.js';
+
+export default class Card {
+  // the contrustor will store dynamic data,
+  // each instance will have its own personal data
+  constructor(data, cardTemplate) {
+    // the name and the link are private fields,
+    // they're only needed inside the class
+    this._name = data.name;
+    this._link = data.link;
+    this._cardTemplate = cardTemplate;
+  }
+
+  _getTemplate() {
+    // taking the markup from HTML and cloning the element
+    const cardElement = document
+      .querySelector(this._cardTemplate)
+      .content.querySelector(cardConfig.cardElement)
+      .cloneNode(true);
+    // return the DOM element of the card
+    return cardElement;
+  }
+
+  generateCard() {
+    // public method that returns a fully functional card populated with data
+    this._element = this._getTemplate();
+    this._image = this._element.querySelector(cardConfig.cardImageElement);
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._element.querySelector(
+      cardConfig.cardTitleElement
+    ).textContent = this._name;
+    this._setEventListeners();
+    return this._element;
+  }
+
+  _handleOpenPopup() {
+    popupConfig.popupFigImg.src = this._link;
+    popupConfig.popupFigImg.alt = this._name;
+    popupConfig.popupFigCaption.textContent = this._name;
+    popupConfig.popupImgElement.classList.add(popupConfig.popupOpenedClass);
+  }
+
+  _handleClosePopup() {
+    popupConfig.popupFigImg.src = '';
+    popupConfig.popupFigImg.alt = '';
+    popupConfig.popupFigCaption.textContent = '';
+    popupConfig.popupImgElement.classList.remove(popupConfig.popupOpenedClass);
+  }
+
+  _handleDeleteCard() {
+    this._element.remove();
+  }
+
+  _handleLikeCard(e) {
+    e.target.classList.toggle(cardConfig.cardLikedClass);
+  }
+
+  _setEventListeners() {
+    this._image.addEventListener('click', () => {
+      // open the popup
+      this._handleOpenPopup();
+    });
+
+    popupConfig.buttonElement.addEventListener('click', () => {
+      // close the popup
+      this._handleClosePopup();
+    });
+
+    this._element
+      .querySelector(cardConfig.cardDeleteElement)
+      .addEventListener('click', () => {
+        this._handleDeleteCard();
+      });
+
+    this._element
+      .querySelector(cardConfig.cardLikeElement)
+      .addEventListener('click', e => {
+        this._handleLikeCard(e);
+      });
+  }
+}
---
+++ src/components/FormValidator.js
@@ -0,0 +1,83 @@
+import { formConfig } from '../index.js';
+
+export default class FormValidator {
+  constructor(config, formElement) {
+    // the name and the link are private fields,
+    // they're only needed inside the class
+    this._config = config;
+    this._formElement = formElement;
+  }
+
+  _showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(formConfig.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(formConfig.errorClass);
+  }
+
+  _hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(formConfig.inputErrorClass);
+    errorElement.classList.remove(formConfig.errorClass);
+    errorElement.textContent = '';
+  }
+
+  _checkInputValidity(formElement, inputElement) {
+    !inputElement.validity.valid
+      ? this._showInputError(
+          formElement,
+          inputElement,
+          inputElement.validationMessage
+        )
+      : this._hideInputError(formElement, inputElement);
+  }
+
+  _hasInvalidInput(inputList) {
+    return inputList.some(inputElement => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  _toggleButtonState(inputList, buttonElement) {
+    if (this._hasInvalidInput(inputList)) {
+      buttonElement.classList.add(formConfig.inactiveButtonClass);
+    } else {
+      buttonElement.classList.remove(formConfig.inactiveButtonClass);
+    }
+  }
+
+  _setEventListeners(formElement) {
+    const inputList = Array.from(
+      formElement.querySelectorAll(formConfig.inputElement)
+    );
+    const buttonElement = formElement.querySelector(formConfig.buttonElement);
+
+    // call here to check button state on page load
+    this._toggleButtonState(inputList, buttonElement);
+    // Iterate over the resulting array
+    inputList.forEach(inputElement => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(formElement, inputElement);
+        // call here, to check button state on when input event is fired
+        this._toggleButtonState(inputList, buttonElement);
+      });
+    });
+  }
+
+  enableValidation() {
+    const formList = Array.from(
+      document.querySelectorAll(formConfig.formElement)
+    );
+    formList.forEach(formElement => {
+      formElement.addEventListener('submit', e => {
+        e.preventDefault();
+      });
+      const fieldsetList = Array.from(
+        formElement.querySelectorAll(formConfig.formSetElement)
+      );
+      fieldsetList.forEach(fieldSet => {
+        this._setEventListeners(fieldSet);
+      });
+    });
+  }
+}
